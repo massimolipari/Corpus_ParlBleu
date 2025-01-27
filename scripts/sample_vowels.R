60d53e55c7654f258ef83784151137f7a92700dd
Massimo Lipari <massimo.lipari@mail.mcgill.ca>
2025-01-26 21:39:04 -0500

    Re-made sample

--- scripts/sample_vowels.R
+++ scripts/sample_vowels.R
@@ -2,17 +2,17 @@ library(tidyverse)
 set.seed(76)
 
 # Load the datasets
-vowels <- read_csv('extract/AssNat_vowels.csv')
-bios <- read_csv('meta/bios.csv')
+vowels <- read_csv('./extract/AssNat_vowels.csv')
+bios <- read_csv('./meta/bios.csv')
 
 # Drop tokens that are too short for good measures, from English borrowings
 vowels_sub <- vowels %>% 
-  filter(phone_duration >= 0.05,
-         !phone %in% c('ɪ', 'ʊ'))
-
-vowels_sub <- vowels_sub %>% 
   left_join(bios, by = c('speaker' = 'id')) %>% 
-  relocate(prénom:lieu, .after = speaker)
+  relocate(prénom:lieu, .after = speaker) %>% 
+  filter(phone_duration >= 0.05,
+         !phone %in% c('ɪ', 'ʊ'),
+         !word %in% c('euh'),
+         lieu == 'Québec')
 
 # Future proofing, in case a larger sample is desired later
 vowels_sample_full <- vowels_sub %>% 
